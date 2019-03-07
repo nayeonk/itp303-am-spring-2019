69b16c62188239a36c5c2b17e7204285fe26668b
nayeonkim <nayeonki@usc.edu>
2019-03-07 14:27:49 -0800

    Update lect 14 with more examples

--- lect14-manipulating-db/manipultation.sql
+++ lect14-manipulating-db/manipultation.sql
@@ -64,9 +64,61 @@ FROM tracks;
 SELECT SUM(milliseconds) FROM tracks
 WHERE album_id = 1;
 
--- Find the shortest track for EACH album
+-- Find the sum of all tracks for EACH album
 SELECT album_id, SUM(milliseconds) 
 FROM tracks
 GROUP BY album_id;
+
+-- EXTRA STATEMENTS not covered in class, but for your reference
+
+-- Create a view name that displays all Rock tracks, sorted from shortest to longest
+-- maybe write the SELECT statement first
+CREATE OR REPLACE VIEW rock_tracks AS
+	SELECT tracks.name AS track_name, genres.name AS genre_name, milliseconds
+    FROM tracks
+    JOIN genres
+		ON genres.genre_id = 1
+	ORDER BY milliseconds;
+
+SELECT * 
+FROM rock_tracks;
+
+-- This shows the shortest track overall
+SELECT name, album_id, MIN(milliseconds)
+FROM tracks;
+
+-- Find shortest track for EACH album
+SELECT album_id, MIN(milliseconds)
+FROM tracks
+GROUP BY album_id;
+
+-- JOIN to see the names of albums
+SELECT albums.title, MIN(milliseconds)
+FROM tracks
+JOIN albums
+	ON tracks.album_id = albums.album_id
+GROUP BY albums.album_id
+
+-- A lil more complex - for each artist, show artists and number of their albums
+SELECT album_id, title, artist_id, COUNT(*)
+FROM albums
+GROUP BY artist_id;
+-- JOIN to show the artist name
+SELECT album_id, artists.name, artists.artist_id, COUNT(*)
+FROM albums
+JOIN artists
+	ON artists.artist_id = albums.artist_id
+GROUP BY artists.artist_id;
+
+-- For each artist, show num of characters for longest album title
+SELECT *, MAX(CHAR_LENGTH(title))
+FROM albums
+GROUP BY artist_id;
+
+SELECT tracks.genre_id, genres.name, MIN(milliseconds), MAX(milliseconds)
+FROM tracks
+JOIN genres
+ON genres.genre_id = tracks.genre_id
+GROUP BY tracks.genre_id;
 
 

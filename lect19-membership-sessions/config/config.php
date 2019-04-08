41f444ffac798d7e35e888612265e6f35c50c9a1
nayeonkim <nayeonki@usc.edu>
2019-04-08 12:35:30 -0700

    Add lect 19 files

---
+++ lect19-membership-sessions/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+// Having session_start here means all pages with config will automatically start sessions
+
+define('DB_HOST', '303.itpwebdev.com');
+define('DB_USER', 'nayeon_db_user');
+define('DB_PASS', 'uscItp2019');
+define('DB_NAME', 'nayeon_song_db');
+
+?>
